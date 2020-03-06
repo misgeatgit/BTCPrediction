cead23ba34d99eab5ddbb8d39579b8a973fc0390
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-03-06 16:27:15 +0800

    Support multiple binarizations and saving to a separate directory

--- MOSES/transformer.py
+++ MOSES/transformer.py
@@ -20,15 +20,15 @@ def binarize(data, config):
         feat_name = dct['name']
         bins_to_try = dct['bins']
         binarizer = BinarizationFactory().get_binarizer(dct['binarizer'])
-        logging.info('Feat: {} Bins: {}'.format(feat_name, bins_to_try))
         binarizations = []
         for bins in bins_to_try:
+            logging.info('Feat: {} Bins: {}'.format(feat_name, bins))
             bin_matrix = binarizer.binarize(data[feat_name].to_numpy(), bins)
             # build dataFrame
             df_columns = {}
             for i in range(bins):
                 df_columns[feat_name+'_bin'+str(i)] = np.array(bin_matrix)[:,i]
-            binarizations.append(pds.DataFrame(df_columns))
+            binarizations.append(('{}Bins{}'.format(feat_name,bins), pds.DataFrame(df_columns))) #name_binarization tuple
         logging.info('Result:\n{}'.format(binarizations))
         feat_binarized_DFs[feat_name] = binarizations
 
@@ -54,18 +54,20 @@ def _cartesian(elements_set):
 
 def combine_all(feat_bin_matrix):
     '''
-    input: A map of feature name and list of binary panda DataFram objects
+    input: A map of feature name and list of tuple of names and binary panda DataFram objects
     output: A list of all combination of binary matrix from concatenating each feat binarization
     '''
     combinations = _cartesian(list(feat_bin_matrix.values()))
     dfs = []
     for combination in combinations:
-        df_merged = combination[0]
-        for df in combination[1:]:
-            assert(df_merged.shape[0] == df.shape[0])
-            df_merged = pds.merge(df_merged, df, left_index=True, right_index=True)
+        df_merged = combination[0][1] #df
+        name = combination[0][0] #name(featNameBins)
+        for name_df in combination[1:]:
+            assert(df_merged.shape[0] == name_df[1].shape[0])
+            name += name_df[0]
+            df_merged = pds.merge(df_merged, name_df[1], left_index=True, right_index=True)
 
-        dfs.append(df_merged)
+        dfs.append((name, df_merged))
 
     return dfs
 
@@ -92,7 +94,7 @@ def main():
        list_of_bin_matrix = combine_all(list_of_feat_bin_matrix)
 
        for matrix in list_of_bin_matrix:
-           logging.info('{}\n'.format(matrix))
+           logging.info('BinarizatinName: {}:\n{}\n'.format(matrix[0],matrix[1]))
 
        target = config['targetFeature']
        predictions = binarize_target(data[target].to_numpy(), config['LA_days'])
@@ -101,8 +103,8 @@ def main():
        for la_days in predictions.keys():
            for i in range(len(list_of_bin_matrix)):
                logging.info('LA_years: {}\n'.format(la_days))
-               logging.info('inputFeature bin_matrix initial size: {}'.format(list_of_bin_matrix[i].shape))
-               df = list_of_bin_matrix[i].drop(list_of_bin_matrix[i].tail(la_days).index,inplace=False)
+               logging.info('inputFeature bin_matrix initial size: {}'.format(list_of_bin_matrix[i][1].shape))
+               df = list_of_bin_matrix[i][1].drop(list_of_bin_matrix[i][1].tail(la_days).index,inplace=False)
                target_feat_bin_vec = pds.DataFrame({target:predictions[la_days]})
                logging.info('inputFeature bin_matrix size after resize: {}'.format(df.shape))
                logging.info('targetFeature bin_matrix initial size: {} \n'.format(target_feat_bin_vec.shape))
@@ -112,21 +114,21 @@ def main():
                        target_feat_bin_vec, left_index = True,\
                        right_index = True)
                if la_days in moses_data.keys():
-                   moses_data[la_days].append(moses_df)
+                   moses_data[la_days].append((list_of_bin_matrix[i][0], moses_df))
                else:
-                   moses_data[la_days] = [moses_df]
+                   moses_data[la_days] = [(list_of_bin_matrix[i][0], moses_df)]
 
        #TODO use informative naming
        for la_days in moses_data.keys():
            cnt = 0
            saving_dir = '{}/LA_{}'.format(dump_dir,la_days)
            os.mkdir(saving_dir)
-           for moses_df in moses_data[la_days]:
-                saving_child_dir = '{}/binarization_{}'.format(saving_dir, cnt)
+           for name_moses_df in moses_data[la_days]:
+                saving_child_dir = '{}/{}'.format(saving_dir, name_moses_df[0])
                 cnt += 1
                 os.mkdir(saving_child_dir)
                 file_name = saving_child_dir + '/data.csv'
-                moses_df.to_csv(file_name, sep=' ', index=False)
+                name_moses_df[1].to_csv(file_name, sep=' ', index=False)
     else:
         parser.print_help()
 
