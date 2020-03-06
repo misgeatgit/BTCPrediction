169321c21c624ee715014510bb843e894cf53ac1
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-03-06 19:35:28 +0800

    Make it pick top 5 MOSES models based on training acc and prec

--- MOSES/rank_results.py
+++ MOSES/rank_results.py
@@ -15,12 +15,11 @@ for LA_exp in LA_exp_dirs:
     LA_exp_path = root_dir + '/'+ LA_exp 
     if not os.path.isdir(LA_exp_path):
         continue
-    max_prec = float('-inf')
-    max_prec_file = None
+    print('Ranking in {}'.format(LA_exp))
     max_prec_df = None
-    max_acc = float('-inf')
-    max_acc_file = None
     max_acc_df = None
+    top_5_max_prec = []
+    top_5_max_acc = []
     path_info = {'top_prec_file':[], 'top_acc_file':[]}
     for exp_dir in os.listdir(LA_exp_path):
         path = LA_exp_path + '/' + exp_dir
@@ -29,28 +28,47 @@ for LA_exp in LA_exp_dirs:
         csvf = path + '/metrics.csv'
         csvf_rel_path =  exp_dir + '/metrics.csv'
         # start ranking by precision here.
-        df = pds.read_csv(csvf).sort_values(by='test_precision', ascending=False)
-        if df['test_precision'][0] > max_prec:
-            max_prec = df['test_precision'][0]
-            max_prec_file = csvf
-            max_prec_df = df
-            if path_info['top_prec_file']:
-                path_info['top_prec_file'][0] = csvf_rel_path
+        # TODO use heap instead of sorting. 5x faster.
+        df_prec = pds.read_csv(csvf).sort_values(by='test_precision', ascending=False)
+        if len(top_5_max_prec) < 5:
+            top_5_max_prec.append([df_prec, csvf_rel_path])
+        else:
+            top_5_max_prec.sort(key=lambda s: s[0].sort_values\
+                    (by='training_precision',
+                     ascending=False)['training_precision'][0], reverse=True)
+            least = top_5_max_prec.pop()
+            if df_prec['training_precision'][0] > least[0]['training_precision'][0]:
+                top_5_max_prec.append([df_prec, csvf_rel_path])
             else:
-                path_info['top_prec_file'].append(csvf_rel_path)
-        df = pds.read_csv(csvf).sort_values(by='test_accuracy', ascending=False)
-        if df['test_accuracy'][0] > max_acc:
-            max_acc = df['test_accuracy'][0]
-            max_acc_file = csvf
-            max_acc_df = df
-            if path_info['top_acc_file']:
-                path_info['top_acc_file'][0] = csvf_rel_path
-            else:
-                path_info['top_acc_file'].append(csvf_rel_path)
+                top_5_max_prec.append(least)
 
-    max_prec_df.to_csv(LA_exp_path+'/top_prec_metrics.csv',sep=',', index=False)
-    max_acc_df.to_csv(LA_exp_path+'/top_acc_metrics.csv',sep=',', index=False)
-    pds.DataFrame(path_info).to_csv(LA_exp_path+'/top_metrics_file_paths.csv', sep=',', index=False)
+        df_acc = pds.read_csv(csvf).sort_values(by='test_accuracy', ascending=False)
+        if len(top_5_max_acc) < 5:
+            top_5_max_acc.append([df_acc, csvf_rel_path])
+        else:
+            top_5_max_acc.sort(key=lambda s: s[0].sort_values\
+                    (by='training_accuracy',
+                     ascending=False)['training_accuracy'][0], reverse=True)
+            least = top_5_max_acc.pop()
+            if df_acc['training_accuracy'][0] > least[0]['training_accuracy'][0]:
+                top_5_max_acc.append([df_acc, csvf_rel_path])
+            else:
+                top_5_max_acc.append(least)
+
+    #TODO merge those top5 DataFrames
+    print('Top accurate on training count: {}'.format(len(top_5_max_acc)))
+    print('Top precise on training count: {}'.format(len(top_5_max_prec)))
+
+    for i in range(5):
+        path_info['top_prec_file'].append(top_5_max_prec[i][1])
+        path_info['top_acc_file'].append(top_5_max_acc[i][1])
+    pds.DataFrame(path_info).to_csv(LA_exp_path+'/top_5_metrics_file_paths.csv', sep=',', index=False)
+
+    max_prec_df = pds.concat([s[0] for s in top_5_max_prec], ignore_index=True)
+    max_acc_df = pds.concat([s[0] for s in top_5_max_acc], ignore_index=True)
+
+    max_prec_df.to_csv(LA_exp_path+'/top_5_prec_metrics.csv',sep=',', index=False)
+    max_acc_df.to_csv(LA_exp_path+'/top_5_acc_metrics.csv',sep=',', index=False)
 
 
 
