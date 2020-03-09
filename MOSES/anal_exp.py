b6f354340ecfc9b8a6f1f4d27e7e365b50aff488
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-03-09 19:21:10 +0800

    Fix indentation

--- MOSES/anal_exp.py
+++ MOSES/anal_exp.py
@@ -1,53 +1,59 @@
 #!/bin/python2.7
 
-import os
-import csv
 import argparse
+import csv
+import os
+import subprocess
+
 
 
 EVAL_TABLE_PATH="/usr/local/bin/eval-table"
 OUT = 'Price'
 def extract_programs(outputf):
-	"""Extract the combo programs from a given moses output file
-	params:
-	moses output file
-        return list of string representation of the programs
-	"""	
-        programs = []
-        with open(outputf,'r') as f:
-	    combo_lines = f.readlines()
-            for combo_line in combo_lines:
-                 combo = combo_line.split(' ',1)[1]
-	         programs.append(combo)
-        return programs
+    """
+    Extract the combo programs from a given moses output file
+    params:
+    moses output file
+    return list of string representation of the programs
+    """	
+    programs = []
+    with open(outputf,'r') as f:
+        combo_lines = f.readlines()
+        for combo_line in combo_lines:
+             combo = combo_line.split(' ',1)[1]
+             programs.append(combo)
+    return programs
 
 def eval_output(ifile,cfile,ofile):
-	"""evaluate the combo program
-	params:
-	actual file,combo file,output file"""
-        print('Evaluating {} on combo {}'.format(ifile, cfile))
-	global EVAL_TABLE_PATH
-	EVAL_TABLE_ARGS = " -i %s -C %s -o %s -u %s "%(ifile,cfile,ofile,OUT)
-	result=os.system(EVAL_TABLE_PATH + " " + EVAL_TABLE_ARGS) 
-	if result!=0:
-		print "error while executing"
-		exit(0)	
+    """
+    evaluate the combo program
+    params:
+    actual file,combo file,output file
+    """
+    global EVAL_TABLE_PATH
+    EVAL_TABLE_ARGS = " -i %s -C %s -o %s -u %s "%(ifile,cfile,ofile,OUT)
+    #print('Executing command: {}'.format(EVAL_TABLE_PATH +' '+EVAL_TABLE_ARGS))
+    result=subprocess.call(EVAL_TABLE_PATH + " " + EVAL_TABLE_ARGS, shell=True) 
+    if result!=0:
+        print("error while executing")
+        exit(0)	
 
 def values_of_col(csvf,col_name,sepchar=' '):
-	"""get values of a given column name from a csv file
-	returns list of the column values without the column name
-	params csv file path,column name,delimiter char in the csv
-	"""
-	col_values=[]
-	with open(csvf,'rb') as f:
-		reader=csv.reader(f,delimiter=sepchar)
-		csv_list=list(reader)
-		header_row=csv_list[0]		
-		col_name_index=header_row.index(col_name)
-		for row in csv_list:
-			col_values.append(row[col_name_index])
-	del col_values[0] # remove the column name		
-	return col_values						
+    """
+    get values of a given column name from a csv file
+    returns list of the column values without the column name
+    params csv file path,column name,delimiter char in the csv
+    """
+    col_values=[]
+    with open(csvf,'r') as f:
+            reader=csv.reader(f,delimiter=sepchar)
+            csv_list=list(reader)
+            header_row=csv_list[0]		
+            col_name_index=header_row.index(col_name)
+            for row in csv_list:
+                    col_values.append(row[col_name_index])
+    del col_values[0] # remove the column name		
+    return col_values						
 def calc_performance(predicted, actual):
     assert len(predicted) == len(actual)
     tp = tn = fp = fn = 0.0       #tp - true positive tn -true negative fp - false positive fn -false negative
@@ -78,7 +84,7 @@ def save_result(data,resultf):
 	writer=csv.writer(resultf)
 	writer.writerows(result_csv)
 if __name__ == "__main__":
-	print "parsing...evaluatin...scoring"
+	print("parsing...evaluatin...scoring")
 	usage = "usage: %prog [options]\n"
 	parser = argparse.ArgumentParser(usage)
 	parser.add_argument("-T", "--trainf",nargs=1,help = "moses training file")
@@ -87,48 +93,48 @@ if __name__ == "__main__":
 	parser.add_argument("-c", "--combof",nargs=1,help = "file where combo program is within")
 	args = parser.parse_args()
 	if args.trainf and args.testf and args.trtstdir and args.combof:
-                # Training and testing files
-                mtrainfname = args.trainf[0]
-                mtestfname = args.testf[0]
-		
-                trtstdir = os.path.abspath(args.trtstdir[0]) # a dir to store eval-table output
-		moses_resf = os.path.abspath(args.combof[0]) #
-		combofp_dir = os.path.split(moses_resf)[0]
-		programs = extract_programs(moses_resf)
-                i = 0
-                perf_metrix_str = 'training_recall,training_accuracy,training_precision,test_recall,test_accuracy,test_precision,program\n'
-                suffix = ''
-                exp_result_dir = ''
-                for program in programs:
-		    combof = os.path.join(combofp_dir,"{}_{}.combo".format(mtrainfname, i))
-		    mtrain_evalf = os.path.join(trtstdir,'{}_{}.eval'.format(mtrainfname, i))
-		    mtest_evalf = os.path.join(trtstdir,'{}_{}.eval'.format(mtestfname, i))
-                    i += 1
-		    # Create a file containing the combo program
-                    with open(combof, 'w') as f:
-                        f.write(program)
-                    # Run eval-table
-		    eval_output(os.path.join(trtstdir,mtrainfname), combof, mtrain_evalf)
-		    eval_output(os.path.join(trtstdir,mtestfname), combof, mtest_evalf)
-                    print("TRAINING METRICS:")
-                    ptr = calc_performance(values_of_col(mtrain_evalf, OUT), \
-                            values_of_col(os.path.join(trtstdir,mtrainfname), OUT))
-                    print("TEST METRICS:")
-                    ptst = calc_performance(values_of_col(mtest_evalf, OUT), \
-                            values_of_col(os.path.join(trtstdir,mtestfname), OUT))
+            # Training and testing files
+            mtrainfname = args.trainf[0]
+            mtestfname = args.testf[0]
 
-                    # Append result of this experiment to results.csv
-                    exp = os.path.split(trtstdir)
-                    pdir = os.path.split(exp[0])[1]
-	            combo = program
-                    line = '{},{},{},{},{},{},{}'.format(round(ptr['recall'],2),\
-                            round(ptr['accuracy'],2),round(ptr['precision'],2),round(ptst['recall'],2),\
-                            round(ptst['accuracy'],2),round(ptst['precision'],2),combo)
-                    # Append to result
-                    perf_metrix_str += line
-                result_f = '{}/metrics.csv'.format(trtstdir)
-                with open(result_f, 'a+') as f:
-                    f.write(perf_metrix_str)
-                print('Result saved to {}'.format(result_f))
+            trtstdir = os.path.abspath(args.trtstdir[0]) # a dir to store eval-table output
+            moses_resf = os.path.abspath(args.combof[0]) #
+            combofp_dir = os.path.split(moses_resf)[0]
+            programs = extract_programs(moses_resf)
+            i = 0
+            perf_metrix_str = 'training_recall,training_accuracy,training_precision,test_recall,test_accuracy,test_precision,program\n'
+            suffix = ''
+            exp_result_dir = ''
+            for program in programs:
+                combof = os.path.join(combofp_dir,"{}_{}.combo".format(mtrainfname, i))
+                mtrain_evalf = os.path.join(trtstdir,'{}_{}.eval'.format(mtrainfname, i))
+                mtest_evalf = os.path.join(trtstdir,'{}_{}.eval'.format(mtestfname, i))
+                i += 1
+                # Create a file containing the combo program
+                with open(combof, 'w') as f:
+                    f.write(program)
+                # Run eval-table
+                eval_output(os.path.join(trtstdir,mtrainfname), combof, mtrain_evalf)
+                eval_output(os.path.join(trtstdir,mtestfname), combof, mtest_evalf)
+                print("TRAINING METRICS:")
+                ptr = calc_performance(values_of_col(mtrain_evalf, OUT), \
+                        values_of_col(os.path.join(trtstdir,mtrainfname), OUT))
+                print("TEST METRICS:")
+                ptst = calc_performance(values_of_col(mtest_evalf, OUT), \
+                        values_of_col(os.path.join(trtstdir,mtestfname), OUT))
+
+                # Append result of this experiment to results.csv
+                exp = os.path.split(trtstdir)
+                pdir = os.path.split(exp[0])[1]
+                combo = program
+                line = '{},{},{},{},{},{},{}'.format(round(ptr['recall'],2),\
+                        round(ptr['accuracy'],2),round(ptr['precision'],2),round(ptst['recall'],2),\
+                        round(ptst['accuracy'],2),round(ptst['precision'],2),combo)
+                # Append to result
+                perf_metrix_str += line
+            result_f = '{}/metrics.csv'.format(trtstdir)
+            with open(result_f, 'a+') as f:
+                f.write(perf_metrix_str)
+            print('Result saved to {}'.format(result_f))
 	else:
 	    parser.print_help()
