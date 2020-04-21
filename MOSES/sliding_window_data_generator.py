4c21b8fbf8a93bfba297d760ebd6d08c9b1aa21e
Misgana Bayetta <misgana.bayetta@gmail.com>
2020-04-21 16:12:52 +0800

    Prototyping

---
+++ MOSES/sliding_window_data_generator.py
@@ -0,0 +1,10 @@
+
+'''
+Generates training-test data by sliding a window of size K
+over the data with each K size being train and K+1th the test.
+
+saves data in the data section
+with a base dir called sliding-window-data
+'''
+
+
